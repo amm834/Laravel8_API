966181e51ea70ba6316bffc6203cc9d7410c0c8c
Aung Myat Moe <aungmyatmoe.edu@gmail.com>
2020-12-10 19:58:44 +0630

    Api:Data Store Method

--- app/Http/Controllers/API/PostController.php
+++ app/Http/Controllers/API/PostController.php
@@ -7,70 +7,70 @@ use Illuminate\Http\Request;
 use App\Models\Post;
 class PostController extends Controller
 {
-	/**
-	* Display a listing of the resource.
-	*
-	* @return \Illuminate\Http\Response
-	*/
-	public function index() {
-		$data = Post::all();
-		return response()->json($data, 200);
-	}
+  /**
+  * Display a listing of the resource.
+  *
+  * @return \Illuminate\Http\Response
+  */
+  public function index() {
+    $data = Post::all();
+    return response()->json($data, 200);
+  }
 
-	/**
-	* Store a newly created resource in storage.
-	*
-	* @param  \Illuminate\Http\Request  $request
-	* @return \Illuminate\Http\Response
-	*/
-	public function store(Request $request) {
-		$data = Post::create([
-			'title'=>$request->get('title'),
-			'description'=>$request->get('description')
-			]);
-			return response()->json($data,200);
-	}
+  /**
+  * Store a newly created resource in storage.
+  *
+  * @param  \Illuminate\Http\Request  $request
+  * @return \Illuminate\Http\Response
+  */
+  public function store(Request $request) {
+    $data = Post::create([
+      'title' => $request->get('title'),
+      'description' => $request->get('description')
+    ]);
+    return response()->json([$data,'msg'=>'Post Created Successfully.'], 200);
+  }
 
-	/**
-	* Display the specified resource.
-	*
-	* @param  int  $id
-	* @return \Illuminate\Http\Response
-	*/
-	public function show($id) {
-	  $data = Post::whereId($id)->firstOrFail();
-	  return response()->json($data,200);
-	}
+  /**
+  * Display the specified resource.
+  *
+  * @param  int  $id
+  * @return \Illuminate\Http\Response
+  */
+  public function show($id) {
+    $data = Post::whereId($id)->firstOrFail();
+    return response()->json($data, 200);
+  }
 
-	/**
-	* Update the specified resource in storage.
-	*
-	* @param  \Illuminate\Http\Request  $request
-	* @param  int  $id
-	* @return \Illuminate\Http\Response
-	*/
-	public function update(Request $request, $id) {
-		$post = Post::whereId($id)->firstOrFail();
-		$post->update([
-			'title'=>$request->get('title'),
-			'description'=>$request->get('description')
-			]);
-			return response()->json([
-				'msg'=>'Update Success'
-				],200);
-	}
+  /**
+  * Update the specified resource in storage.
+  *
+  * @param  \Illuminate\Http\Request  $request
+  * @param  int  $id
+  * @return \Illuminate\Http\Response
+  */
+  public function update(Request $request, $id) {
+    $post = Post::whereId($id)->firstOrFail();
+    $post->update([
+      'title' => $request->get('title'),
+      'description' => $request->get('description')
+    ]);
+    return response()->json([
+      'msg' => 'Update Success'
+    ], 200);
+  }
 
-	/**
-	* Remove the specified resource from storage.
-	*
-	* @param  int  $id
-	* @return \Illuminate\Http\Response
-	*/
-	public function destroy($id) {
-		$post = Post::whereId($id)->firstOrFail();
-		$post->delete();
-		return response()->json([
-			'msg'=>'Delete Success'
-			],200);
-	}
+  /**
+  * Remove the specified resource from storage.
+  *
+  * @param  int  $id
+  * @return \Illuminate\Http\Response
+  */
+  public function destroy($id) {
+    $post = Post::whereId($id)->firstOrFail();
+    $post->delete();
+    return response()->json([
+      'msg' => 'Delete Success'
+    ], 200);
+  }
 }
---
+++ public/js/app.js
@@ -0,0 +1,54 @@
+/* Init of Eruda */
+eruda.init();
+
+/* api */
+let tbody = document.querySelector("#tbody");
+axios.get("/api/posts")
+.then((response)=> {
+  response.data.forEach((posts)=> {
+    tbody.innerHTML += `
+    <tr>
+    <th scope="row">${posts.id}</th>
+    <td>Noope World!</td>
+    <td>${posts.description}</td>
+    <td>
+    <a href="" class="btn btn-outline-info btn-sm">
+    Edit
+    </a>
+    </td>
+    <td>
+    <a href="" class="btn btn-danger btn-sm">
+    Delete
+    </a>
+    </td>
+    </tr>
+
+    `;
+  })
+})
+.catch((errors)=>console.log(errors));
+
+/* Data Insert */
+let postInputForm = document.forms['postInputForm'];
+let titleInput = postInputForm['title'];
+let descInput = postInputForm['description'];
+postInputForm.onsubmit = function(e) {
+  e.preventDefault();
+  axios.post('/api/posts', {
+    'title': titleInput.value,
+    'description': descInput.value
+  })
+  .then((res)=> {
+    let successMsg = `
+    <div class="alert alert-success alert-dismissible fade show" role="alert">
+    ${res.data.msg}
+  <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+</div>
+    `;
+    document.querySelector('#status').innerHTML = successMsg;
+    console.log(res);
+  })
+  .catch((errors)=> {
+    console.log(errors.response)
+  })
+}
